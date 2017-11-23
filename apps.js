1d15fc01c08bc2a12972a98eaa3ce329d7e00d4a
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2017-11-22 14:38:09 -1000

    update 1

---
+++ apps.js
@@ -0,0 +1,12 @@
+//Alien Variables//
+var aliens = 10000//a
+var r = 5500;//x
+var g = 3255;//y
+var b = 1245;//z
+
+function checklist(x, y, z){
+	if (a == 10000){
+		output = x + y + z;
+	}
+		else if ()
+}
