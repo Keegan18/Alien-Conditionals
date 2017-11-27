df9d91b6f73f42c8b2f9d9e57d34ffef37366ac0
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2017-11-27 11:23:31 -1000

    Update 3

--- apps.js
+++ apps.js
@@ -1,23 +1,25 @@
 //Alien Variables//
 var aliens = 10000;//a
-var r = 5500;//x
+var b = 1245;//x
 var g = 3255;//y
-var b = 1245;//z
+var r = 5500;//z
 
 function checklist(x, y, z){
-	if (a == 10000){
-		output = x + y + z;
-	}	//Alien total check
-			else if (y <= 5000){
-				output = "MOTHER OF GOD, not the green ones";
-			}
-			else if (x + y <= 8000){
-				output = "DOOOOOM";
-			}
-			else if (x + z >= 3000){
-				output = "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!";
-			}
+	if (a == 10000){ //Alien total check
+		output = x + y + z; 
+	}
+	
+	if (y >= 5000){ //Green Alien #
+		console.log("MOTHER OF GOD, not the green ones");
+	}
+	
+	if (y + z >= 8000){ //Green and Red Alien #
+		console.log("DOOOOOM");
+	}
+	
+	if (x + z <= 3000){ //Red and Blue Alien #
+		console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
+	}
 }
-console.log("MOTHER OF GOD, not the green ones");
-console.log("DOOOOOM");
-console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
+
+checklist(b,g,r);
