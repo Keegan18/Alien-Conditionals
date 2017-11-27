1359b62ea3e3205da0f03a77356f5071990a5902
unknown <kolbeinsen.tafaoa@gmail.com>
2017-11-26 23:15:27 -1000

    Update 2

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
 //Alien Variables//
-var aliens = 10000//a
+var aliens = 10000;//a
 var r = 5500;//x
 var g = 3255;//y
 var b = 1245;//z
@@ -7,6 +7,17 @@ var b = 1245;//z
 function checklist(x, y, z){
 	if (a == 10000){
 		output = x + y + z;
-	}
-		else if ()
+	}	//Alien total check
+			else if (y <= 5000){
+				output = "MOTHER OF GOD, not the green ones";
+			}
+			else if (x + y >= 8000){
+				output = "DOOOOOM";
+			}
+			else if (x + z >= 3000){
+				output = "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!";
+			}
 }
+console.log("MOTHER OF GOD, not the green ones");
+console.log("DOOOOOM");
+console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
